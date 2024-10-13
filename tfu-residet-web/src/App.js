8906934caa674efe0be88b2f4646aa67e6d7d614
anhbthe170035 <anhbthe170035@fpt.edu.vn>
2024-10-13 16:36:56 +0700

    feat: config helmet for SEO

--- tfu-residet-web/src/App.js
+++ tfu-residet-web/src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
+import { Helmet } from "react-helmet";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import ForgotPassword from "./pages/ForgotPassword";
@@ -10,6 +11,18 @@ import { routeArray } from "./constants/routes";
 function App() {
   return (
     <Router>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>TFU Resident - CMS</title>
+        <meta
+          name="description"
+          content="Chuyển đổi số mô hình quản lý-Thông tin dữ liệu chung cư-căn hộ,cư dân;Số hóa phiếu thu, thông báo phí, thanh toán online; Tương tác hai chiều; Đặt dịch vụ, tiện ích (đánh giá chất lượng); Truyền thông cư dân; Sổ tay cư dân, danh bạ,…;Khách thăm &amp; kiểm soát ra vào."
+        />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge"></meta>
+        <meta name="robots" content="noindex"></meta>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta property="og:type" content="website"></meta>
+      </Helmet>
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
@@ -23,7 +36,11 @@ function App() {
                 <Header />
                 <Routes>
                   {routeArray.map((item) => (
-                    <Route key={item.route} path={item.route} element={item.component} />
+                    <Route
+                      key={item.route}
+                      path={item.route}
+                      element={item.component}
+                    />
                   ))}
                 </Routes>
               </div>
