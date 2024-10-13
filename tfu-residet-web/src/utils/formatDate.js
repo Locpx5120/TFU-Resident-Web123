2e366e326f84d41a67922f12c4c22c1351b93d57
anhbthe170035 <anhbthe170035@fpt.edu.vn>
2024-10-13 17:51:17 +0700

    feat: format date util

--- tfu-residet-web/src/utils/formatDate.js
+++ tfu-residet-web/src/utils/formatDate.js
@@ -1,1 +1,47 @@
-//Hàm format ngày tháng
+import dayjs from 'dayjs';
+import utc from 'dayjs/plugin/utc';
+import timezone from 'dayjs/plugin/timezone';
+import localizedFormat from 'dayjs/plugin/localizedFormat';
+import 'dayjs/locale/vi';
+import 'dayjs/locale/en';
+import 'dayjs/locale/id';
+// import i18n from 'i18next';
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
+dayjs.extend(localizedFormat);
+
+const languageToTimezone = {
+  vi: 'Asia/Ho_Chi_Minh',
+  en: 'America/New_York',
+  id: 'Asia/Jakarta'
+};
+
+const languageToLocale = {
+  vi: 'vi',
+  en: 'en',
+  id: 'id'
+};
+
+export function formatDate(date, format = 'L LTS') {
+    //   const lang = i18n.language;
+    const lang = 'vi';
+  const timezone = languageToTimezone[lang] || 'UTC';
+  const locale = languageToLocale[lang] || 'en';
+
+  dayjs.locale(locale);
+
+  let formattedDate = dayjs(date).tz(timezone);
+
+  if (lang === 'vi') {
+    format = format.replace('L', 'DD/MM/YYYY');
+  } else if (lang === 'id') {
+    format = format.replace('L', 'DD/MM/YYYY');
+  }
+
+  return formattedDate.format(format);
+}
+
+export function getCurrentDateTime(format = 'L LTS') {
+  return formatDate(new Date(), format);
+}
