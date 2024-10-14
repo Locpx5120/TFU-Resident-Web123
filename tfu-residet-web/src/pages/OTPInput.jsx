e1dfcebac8d4431d4384888aca327d11e5580f5c
anhbthe170035 <anhbthe170035@fpt.edu.vn>
2024-10-14 14:37:15 +0700

    feat: forgot pass and otp

--- tfu-residet-web/src/pages/OTPInput.jsx
+++ tfu-residet-web/src/pages/OTPInput.jsx
@@ -1,62 +1,96 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef } from "react";
+import { useParams } from "react-router-dom";
+import axiosInstance from "../config/axiosConfig";
 
 const OTPInput = () => {
-    const [otp, setOtp] = useState(new Array(6).fill(""));
+  const [otp, setOtp] = useState(new Array(6).fill(""));
     const inputsRef = useRef([]);
+    const { id } = useParams();
 
-    // Hàm xử lý thay đổi khi người dùng nhập mã OTP
-    const handleChange = (element, index) => {
-        if (isNaN(element.value)) return;
+  // Hàm xử lý thay đổi khi người dùng nhập mã OTP
+  const handleChange = (element, index) => {
+    if (isNaN(element.value)) return;
 
-        const newOtp = [...otp];
-        newOtp[index] = element.value;
-        setOtp(newOtp);
+    const newOtp = [...otp];
+    newOtp[index] = element.value;
+    setOtp(newOtp);
 
-        // Tự động focus vào ô input tiếp theo
-        if (element.nextSibling && element.value) {
-            inputsRef.current[index + 1].focus();
-        }
-    };
+    // Tự động focus vào ô input tiếp theo
+    if (element.nextSibling && element.value) {
+      inputsRef.current[index + 1].focus();
+    }
+  };
 
-    // Hàm xử lý khi người dùng nhấn backspace để xóa
-    const handleBackspace = (element, index) => {
-        if (element.previousSibling && !element.value) {
-            inputsRef.current[index - 1].focus();
-        }
-    };
+  // Hàm xử lý khi người dùng nhấn backspace để xóa
+  const handleBackspace = (element, index) => {
+    if (element.previousSibling && !element.value) {
+      inputsRef.current[index - 1].focus();
+    }
+  };
 
-    // Hàm xử lý khi người dùng submit mã OTP
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        alert(`Mã OTP của bạn là: ${otp.join('')}`);
-    };
+  // Hàm xử lý khi người dùng submit mã OTP
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+      // alert(`Mã OTP của bạn là: ${otp.join("")}`);
+      const payload = {
+          userId: id,
+          typeOtp: "string",
+          otp: otp.join(""),
+      };
+      const res = await axiosInstance.post(`/auth/confirm-otp`, JSON.stringify(payload));
+      if (res.status === 200) {
+          alert("Xác thực mã OTP thành công");
+      }
+  };
 
-    return (
-        <form onSubmit={handleSubmit}>
-            <h2>Nhập mã OTP</h2>
-            <div>
-                {otp.map((data, index) => (
-                    <input
-                        key={index}
-                        type="text"
-                        maxLength="1"
-                        value={data}
-                        onChange={e => handleChange(e.target, index)}
-                        onKeyDown={e => e.key === "Backspace" && handleBackspace(e.target, index)}
-                        ref={(el) => (inputsRef.current[index] = el)}
-                        style={{
-                            width: "40px",
-                            height: "40px",
-                            margin: "5px",
-                            textAlign: "center",
-                            fontSize: "20px"
-                        }}
-                    />
-                ))}
-            </div>
-            <button type="submit">Xác nhận</button>
-        </form>
-    );
+  return (
+    <form
+      onSubmit={handleSubmit}
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        gap: 10,
+      }}
+    >
+      <h2>Nhập mã OTP</h2>
+          <div style={{
+              display: 'flex',
+              justifyContent: "center",
+      }}>
+        {otp.map((data, index) => (
+          <input
+            key={index}
+            type="text"
+            maxLength="1"
+            value={data}
+            onChange={(e) => handleChange(e.target, index)}
+            onKeyDown={(e) =>
+              e.key === "Backspace" && handleBackspace(e.target, index)
+            }
+            ref={(el) => (inputsRef.current[index] = el)}
+            style={{
+              width: "40px",
+              height: "40px",
+              margin: "5px",
+              textAlign: "center",
+              fontSize: "20px",
+            }}
+          />
+        ))}
+      </div>
+          <button style={{
+              background: '#2ca8a2',
+              padding: '10px 40px',
+              border: 'none',
+              outline: 'none',
+              appearance: 'none',
+              borderRadius: 5, color: '#fff',
+              cursor: 'pointer',
+      }} type="submit">Xác nhận</button>
+    </form>
+  );
 };
 
 export default OTPInput;
